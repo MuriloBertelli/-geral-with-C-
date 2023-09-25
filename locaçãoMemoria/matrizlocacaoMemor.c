02f22ce504d9185ee495b015fbc88a938f1dff8a
MuriloBertelli <mrlbertelli@gmail.com>
2023-09-25 11:58:51 -0300

    geral

---
+++ locaçãoMemoria/matrizlocacaoMemor.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h> 
+
+
+
+void int_mat(int valor, int *p,int n, int m){
+    int i, j;
+    for( i =0 ; i < n; i++)
+        for(j=0 ; j < m ; j++ )
+            *(p+i*m+j)=valor; 
+}
+
+void print_mat(int *p,int n,int m){
+    int i,j;
+    printf("----------------------------------------------");
+    for (i=0;i<n;i++) {
+        printf("\n");
+        for (j=0;j<m;j++)
+            printf("%-5d",*(p+i*m+j));
+}
+printf("\n----------------------------------------------");
+}
+
